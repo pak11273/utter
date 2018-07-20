d06f0897dd04420709952f50aeb56b13de6e0878
pak11273 <pak11273@gmail.com>
2018-07-20 14:19:53 +0000

    adds back secrets file

---
+++ client/src/config/secrets.js
@@ -0,0 +1,11 @@
+import path from 'path'
+
+export default {
+  pixabay: '5532483-c755679d666e745a86ca6934e',
+  yandex:
+    'trnsl.1.1.20170912T012330Z.18385420296d92de.5c552e92f8c2218a1856728cbe8360d856bf0a08',
+  // cdn: '/cdn'
+  cdn: 'http://res.cloudinary.com/dgvw5b6pf/video/upload'
+  // cdn: 'https://cdn.utter.com/uploads'
+  // TODO: add cname and use aws for resource
+}
---
+++ server/src/config/secrets.js
@@ -0,0 +1,15 @@
+module.exports = {
+  jwt: 'gumball',
+  facebook: {
+    clientID: '218390765395433',
+    clientSecret: '621df9fb56685485b52033ac147bef73',
+    profileField: ['email', 'displayName', 'photos'],
+    callbackUrl: 'http://192.168.68.8:3030/auth/facebook/callback',
+    passReqToCallback: true
+  },
+  google: {
+    clientID:
+      '598278254339-2fdd4dfnr8gte0jm4a5p7f3s9p9jpprb.apps.googleusercontent.com',
+    clientSecret: 'WOVF7nS1hMOaIg5APkEssVZW'
+  }
+}
