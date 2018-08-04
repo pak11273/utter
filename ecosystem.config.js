6eb7f8d22d0a59e26340c51f0e2bbca7a08c3406
pak11273 <pak11273@gmail.com>
2018-08-04 15:33:37 +0000

    changed port to 3001 on ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,11 +5,11 @@ module.exports = {
       script: './server/dist/index.js',
       watch: true,
       env: {
-        PORT: 3001,
+        PORT: 3000,
         NODE_ENV: 'development'
       },
       env_production: {
-        PORT: 80,
+        PORT: 3001,
         NODE_ENV: 'production'
       }
     }
