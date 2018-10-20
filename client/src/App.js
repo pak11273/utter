f3feccd698267fee31ef2c601f0391cd1bbcdafc
pak11273 <pak11273@gmail.com>
2018-10-20 00:35:05 +0000

    test: /graphql

--- client/src/App.js
+++ client/src/App.js
@@ -91,7 +91,7 @@ function logPageView() {
 //   ? (GRAPHQL_URL = "http://localhost:3001/graphql")
 //   : (GRAPHQL_URL = "http://192.168.68.8:3001/graphql")
 
-let GRAPHQL_URL = "http://localhost:3001/graphql"
+let GRAPHQL_URL = "/graphql"
 
 const cache = new InMemoryCache({
   cacheRedirects: {
