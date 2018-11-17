67240a43bc6b1d02feb9a8bf04cfce3cade464d5
pak11273 <pak11273@gmail.com>
2018-11-17 03:24:11 +0000

    restructure to packages

---
+++ packages/client/src/layouts/Signup/SignupCtrl.js
@@ -0,0 +1,19 @@
+/* eslint react/no-unescaped-entities: 0 */
+/* eslint react/prefer-stateless-function: 0 */
+
+import React, {PureComponent} from "react"
+import {SignupConnector} from "@utterzone/connector"
+
+import SignupView from "./SignupView.js"
+
+export default class SignupCtrl extends PureComponent {
+  render() {
+    return (
+      <React.Fragment>
+        <SignupConnector>
+          {({submit}) => <SignupView submit={submit} />}
+        </SignupConnector>
+      </React.Fragment>
+    )
+  }
+}
