045c592faf02aa99a86ea16ea0d8093536ccb344
pak11273 <pak11273@gmail.com>
2018-11-18 20:57:53 +0000

    server deployed

--- deploy_server_do.sh
+++ deploy_server_do.sh
@@ -2,6 +2,13 @@
 # login in first to docker with command: docker login
 yarn babel
 
+/usr/bin/expect <<EOD
+spawn ssh -oStrictHostKeyChecking=no -oCheckHostIP=no usr@$myhost.example.com'
+expect "Enter passphrase for key '/home/vagrant/.ssh/id_rsa':"
+send "redeemer\n" 
+expect eof
+EOD
+
 git add .
 git cm -m 'server deployed'
 git push
@@ -9,4 +16,3 @@ git push
 docker build -t pak11273/utterzone-server:latest .
 docker push pak11273/utterzone-server:latest
 ssh root@104.248.209.128 "docker pull pak11273/utterzone-server:latest && docker tag pak11273/utterzone-server:latest dokku/utterzone-server:latest && dokku tags:deploy utterzone-server latest"
-
