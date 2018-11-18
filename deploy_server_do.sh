2eec3179adc8f7edaf54062847df0581891d6667
pak11273 <pak11273@gmail.com>
2018-11-18 20:13:24 +0000

    server deployed

--- deploy_server_do.sh
+++ deploy_server_do.sh
@@ -1,15 +1,10 @@
+#! /bin/bash 
 # login in first to docker with command: docker login
-
-#! /bin/bash
 yarn babel
 
 git add .
 git cm -m 'server deployed'
 git push
-
-#! /usr/bin/expect -f
-expect "Enter passphrase for key '/home/vagrant/.ssh/id_rsa'"
-send "redeemer\r"
 
 docker build -t pak11273/utterzone-server:latest .
 docker push pak11273/utterzone-server:latest
