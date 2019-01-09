7f887b29dd1519ace39bd8f05962168da45fc45a
pak11273 <pak11273@gmail.com>
2019-01-09 19:38:20 +0000

    server deployed

--- packages/server/dist/socketio/zone-chat.js
+++ packages/server/dist/socketio/zone-chat.js
@@ -7,5 +7,9 @@ Object.defineProperty(exports, "__esModule", {
 exports.default = function (socketio) {
   socketio.on("connection", function (socket) {
     console.log("User connected");
+
+    socket.on("createMessage", function (data) {
+      console.log("val: ", data);
+    });
   });
 };
