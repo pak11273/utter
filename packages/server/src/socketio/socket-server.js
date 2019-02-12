13b21ad68b004871dee2b9222e1ff348fe459f51
pak11273 <pak11273@gmail.com>
2019-02-12 18:35:48 +0000

    no duplicate users in zones

--- packages/server/src/socketio/socket-server.js
+++ packages/server/src/socketio/socket-server.js
@@ -4,15 +4,14 @@ import ss from "socket.io-stream"
 import SocketManager from "./socket-mgr.js"
 import ZoneManager from "./zone-mgr.js"
 import makeHandlers from "./handlers"
-import Users from './users.js'
+import Users from "./users.js"
 
 export default async server => {
   const io = socketio(server)
 
   const socketManager = SocketManager()
   const zoneManager = await ZoneManager()
-	const users = new Users
-
+  const users = new Users()
 
   io.on("connection", socket => {
     const {
@@ -44,12 +43,22 @@ export default async server => {
       cb(err, msgObj)
     })
 
-    socket.on("join", (zone,username, cb) => {
+    socket.on("join", (zone, username, cb) => {
       socket.join(zone.id)
-			users.addUserData(zone.id, zone.zoneName, username)
-			io.to(zone.id).emit('usersList', users.getUsersList(zone.id))	
-
+      users.addUserData(socket.id, zone.id, zone.zoneName, username)
+      io.to(zone.id).emit("usersList", users.getUsersList(zone.id))
       cb()
+    })
+
+    socket.on("disconnect", () => {
+      console.log("users: ", users)
+      console.log("SOCKET: ", socket.id)
+      var user = users.removeUserId(socket.id)
+      if (user) {
+        io.to(user.zoneId).emit("usersList", users.getUsersList(user.zoneId))
+      }
+      console.log("socket disconnect...", socket.id)
+      handleDisconnect()
     })
 
     // TODO: NEW API BELOW
@@ -66,19 +75,19 @@ export default async server => {
 
     socket.on("availableUsers", handleGetAvailableUsers)
 
-    socket.on("disconnect", function() {
-      console.log("socket disconnect...", socket.id)
-      handleDisconnect()
-    })
+    /* socket.on("disconnect", () => { */
+    /*   console.log("socket disconnect...", socket.id) */
+    /*   handleDisconnect() */
+    /* }) */
 
     socket.on("error", function(err) {
       console.log("received error from socket:", socket.id)
       console.log(err)
     })
 
-    socket.on("disconnect", socket => {
-      console.log("user disconnected")
-      io.emit("disconnect", {status: "disconnected"})
-    })
+    /* socket.on("disconnect", socket => { */
+    /*   console.log("user disconnected") */
+    /*   io.emit("disconnect", {status: "disconnected"}) */
+    /* }) */
   })
 }
--- packages/server/src/socketio/users.js
+++ packages/server/src/socketio/users.js
@@ -3,8 +3,9 @@ class Users {
     this.users = []
   }
 
-  addUserData(zoneId, zoneName, username) {
+  addUserData(socketId, zoneId, zoneName, username) {
     var users = {
+      socketId,
       zoneId,
       zoneName,
       username
@@ -13,12 +14,28 @@ class Users {
     return users
   }
 
-	getUsersList(zoneId) {
-		var users  = this.users.filter((user)=> user.zoneId === zoneId )
+  removeUserId(socketId) {
+    var user = this.getUser(socketId)
+    if (user) {
+      this.users = this.users.filter(user => user.socketId !== socketId)
+      console.log("users after get him: ", this.users)
+    }
+    return user
+  }
 
-		var namesArr = users.map((user) => user.username)
-		return namesArr
-	}
+  getUser(socketId) {
+    var getUser = this.users.filter(user => {
+      return user.socketId === socketId
+    })[0]
+    return getUser
+  }
+
+  getUsersList(zoneId) {
+    var users = this.users.filter(user => user.zoneId === zoneId)
+
+    var namesArr = users.map(user => user.username)
+    return namesArr
+  }
 }
 
 export default Users
