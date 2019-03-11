2f9e0cc27718029b8392dd3999c57e91448a1160
pak11273 <pak11273@gmail.com>
2019-03-11 15:38:06 +0000

    removes redux from requireAuth

--- packages/client/src/utils/requireAuth.js
+++ packages/client/src/utils/requireAuth.js
@@ -1,21 +1,14 @@
-import React, {Component} from "react"
-import {addFlashMessage} from "../core/actions/flashMessages.js"
-import {connect} from "react-redux"
-import {history} from "@utterzone/connector"
+import React, {PureComponent} from "react"
 import {local} from "brownies"
 
 const requireAuth = WrappedComponent => {
-  class Wrap extends Component {
+  class Wrap extends PureComponent {
     componentDidMount() {}
 
     render() {
       const isAuthenticated = local.AUTH_TOKEN
       if (!isAuthenticated) {
-        /* this.props.addFlashMessage({ */
-        /*   type: "error", */
-        /*   text: "You need to login to access this page" */
-        /* }) */
-        history.push("/login", {
+        this.props.history.push("/login", {
           notification: "You need to login to access this page."
         })
       }
@@ -23,14 +16,7 @@ const requireAuth = WrappedComponent => {
     }
   }
 
-  const actions = {
-    addFlashMessage
-  }
-
-  return connect(
-    null,
-    actions
-  )(Wrap)
+  return Wrap
 }
 
 export default requireAuth
