b5dd9d665d092809410656c9d9ebbf76841f1519
pak11273 <pak11273@gmail.com>
2019-03-25 00:48:52 +0000

    server deployed

--- packages/server/dist/api/term/term-model.js
+++ packages/server/dist/api/term/term-model.js
@@ -11,7 +11,7 @@ var _userModel = _interopRequireWildcard(require("../user/user-model.js"));
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
 
-var TermSchema = new _mongoose.Schema({
+var TermSchema = new _mongoose.default.Schema({
   term: {
     type: String,
     default: "Change me"
