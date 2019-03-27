275c216a497a31525db95b15052e6d0458221007
pak11273 <pak11273@gmail.com>
2019-03-27 03:28:40 +0000

    rebuild course modal delete

--- packages/client/src/containers/modals/course-modal.js
+++ packages/client/src/containers/modals/course-modal.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react"
+import {withRouter} from "react-router-dom"
 import {withFormik} from "formik"
 import Button from "@material-ui/core/Button"
 import Dialog from "@material-ui/core/Dialog"
@@ -6,8 +7,7 @@ import DialogActions from "@material-ui/core/DialogActions"
 import DialogContent from "@material-ui/core/DialogContent"
 import DialogContentText from "@material-ui/core/DialogContentText"
 import DialogTitle from "@material-ui/core/DialogTitle"
-import {graphql, Mutation} from "react-apollo"
-/* import {history} from "@utterzone/connector" */
+import {compose, graphql} from "react-apollo"
 import {session} from "brownies"
 import gql from "graphql-tag"
 import {toast} from "react-toastify"
@@ -19,55 +19,41 @@ const COURSE_DELETE = gql`
 `
 
 export class ModalContainer extends Component {
-  /* deleteCourse = () => { */
-  /*   this.props.deleteCourse(this.props.course) */
-  /* } */
-
   render() {
     const {handleSubmit} = this.props
     const {open, onClose} = this.props
-    const {course} = this.props
     return (
-      <Mutation mutation={COURSE_DELETE}>
-        {courseDelete => (
-          <Dialog
-            aria-labelledby="simple-dialog-title"
-            open={open}
-            onClose={onClose}>
-            <DialogTitle id="simple-dialog-title">Delete Course</DialogTitle>
-            <DialogContent>
-              <DialogContentText id="alert-dialog-description">
-                Deleting this course is irreversible. Click &apos;Delete&apos;
-                to permanently remove this course from the database.
-              </DialogContentText>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={onClose}>Cancel</Button>
-              <form onSubmit={handleSubmit}>
-                <Button
-                  type="Submit"
-                  onClick={e => {
-                    e.preventDefault()
-                    courseDelete({
-                      variables: {courseId: course._id}
-                    })
-                  }}>
-                  Delete
-                </Button>
-              </form>
-            </DialogActions>
-          </Dialog>
-        )}
-      </Mutation>
+      <Dialog
+        aria-labelledby="simple-dialog-title"
+        open={open}
+        onClose={onClose}>
+        <DialogTitle id="simple-dialog-title">Delete Course</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Deleting this course is irreversible. Click &apos;Delete&apos; to
+            permanently remove this course from the database.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={onClose}>Cancel</Button>
+          <form onSubmit={handleSubmit}>
+            <Button onClick={onClose} type="Submit">
+              Delete
+            </Button>
+          </form>
+        </DialogActions>
+      </Dialog>
     )
   }
 }
 
-export default graphql(COURSE_DELETE)(
+export default compose(
+  withRouter,
+  graphql(COURSE_DELETE),
   withFormik({
-    /* mapPropsToValues: () => ({ */
-    /*   resourceId: "" */
-    /* }), */
+    mapPropsToValues: () => ({
+      resourceId: ""
+    }),
     handleSubmit: async (values, {props}, setErrors) => {
       const {course} = session
       const result = await props.mutate({
@@ -77,9 +63,8 @@ export default graphql(COURSE_DELETE)(
       })
 
       const onComplete = () => {
-        console.log("props: ", props)
-        props.actions.closeModal()
-        history.push("/courses/created")
+        delete session.course
+        props.history.push("/courses/created")
       }
 
       // if delete was successfull
@@ -93,9 +78,8 @@ export default graphql(COURSE_DELETE)(
           hideProgressBar: true
         })
       } else {
-        console.log("result.data.errors: ", result.data.errors)
         setErrors(result.data.errors)
       }
     }
-  })(ModalContainer)
-)
+  })
+)(ModalContainer)
