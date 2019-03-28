8c2f9d6b23c7a53c1a3fa08f0df8fc6572a32260
pak11273 <pak11273@gmail.com>
2019-03-28 12:56:28 +0000

    server deployed

--- packages/client/dist/bundle.npm.isobject.e6b95ee1985e80c641a0.js
+++
@@ -1,8 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[51],{xnhM:function(n,o,r){"use strict";
-/*!
- * isobject <https://github.com/jonschlinkert/isobject>
- *
- * Copyright (c) 2014-2017, Jon Schlinkert.
- * Released under the MIT License.
- */n.exports=function(n){return null!=n&&"object"==typeof n&&!1===Array.isArray(n)}}}]);
-//# sourceMappingURL=bundle.npm.isobject.e6b95ee1985e80c641a0.js.map
