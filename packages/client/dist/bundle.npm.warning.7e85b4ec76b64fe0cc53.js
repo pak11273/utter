5e33432c5226232b73dfbd03885275c40b5f4d14
pak11273 <pak11273@gmail.com>
2019-04-10 17:30:11 +0000

    fixed course update

--- packages/client/dist/bundle.npm.warning.7e85b4ec76b64fe0cc53.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[98],{pjAE:function(n,o,p){"use strict";var s=function(){};n.exports=s}}]);
-//# sourceMappingURL=bundle.npm.warning.7e85b4ec76b64fe0cc53.js.map
--- packages/client/src/layouts/courses/containers/course-update.js
+++ packages/client/src/layouts/courses/containers/course-update.js
@@ -10,38 +10,180 @@ import ListItem from "@material-ui/core/ListItem"
 /* import ListItemText from "@material-ui/core/ListItemText" */
 import Typography from "@material-ui/core/Typography"
 import {withStyles} from "@material-ui/core/styles"
-import {styles} from "../styles.js"
 
-class CourseUpdate extends Component {
-  constructor() {
-    super()
-    this.state = {dataProvider: null}
+import styled from "styled-components"
+import cloneDeep from "lodash/cloneDeep"
+import {Helmet} from "react-helmet-async"
+import {Can, Spacer} from "../../../components"
+
+/* const getCourse = gql` */
+/*   query getCourse($_id: ID!) { */
+/*     getCourse(_id: $_id) { */
+/*       _id */
+/*       courseImage */
+/*       title */
+/*       courseMode */
+/*       owner { */
+/*         username */
+/*       } */
+/*     } */
+/*   } */
+/* ` */
+
+const StyledNavLink = styled(NavLink)`
+  grid-area: ${props => props.gridarea};
+  color: #003478;
+  &:hover {
+    color: red;
+    text-decoration: underline;
+  }
+`
+const drawerWidth = 240
+
+const styles = theme => ({
+  content: {
+    alignItems: "center",
+    display: "flex",
+    flexGrow: 1,
+    justifyContent: "center",
+    padding: theme.spacing.unit * 3
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0
+  },
+  drawerPaper: {
+    width: drawerWidth
+  },
+  list: {
+    margin: "0 auto"
+  },
+  root: {
+    display: "flex",
+    flexGrow: 1,
+    width: "100%"
+  }
+})
+
+const initialCoursesContainerState = {
+  resources: ""
+}
+
+class CourseUpdate extends PureComponent {
+  locationName = this.props.path
+
+  constructor(props) {
+    super(props)
+
+    this.state = cloneDeep(initialCoursesContainerState)
   }
 
-  componentDidMount() {
-    buildGraphQLProvider({
-      clientOptions: {
-        uri:
-          process.env.NODE_ENV === "production" ||
-          process.env.NODE_ENV === "prod"
-            ? /* ? process.env.REACT_APP_SERVER_URL */
-              "https://api.utterzone.com/graphql"
-            : "http://192.168.68.8:3010/graphql"
-      }
-    }).then(dataProvider => this.setState({dataProvider}))
+  componentDidMount = async () => {}
+
+  handleImageClick = e => {
+    e.preventDefault()
+    // TODO
   }
 
   render() {
-    const {dataProvider} = this.state
-
-    if (!dataProvider) {
-      return <div>Loading</div>
-    }
-
+    const {course, user} = session
+    const {classes} = this.props
+    const SubRoutes = route => (
+      <Route
+        path={route.path}
+        render={props => <route.component {...props} routes={route.routes} />}
+      />
+    )
+    const {routes} = this.props
     return (
-      <Admin dataProvider={dataProvider}>
-        <Resource name="Post" list={ListGuesser} />
-      </Admin>
+      <div className={classes.root}>
+        <Drawer
+          className={classes.drawer}
+          variant="permanent"
+          classes={{
+            paper: classes.drawerPaper
+          }}>
+          <Spacer margin="200px 0 0 0" />
+          <List className={classes.list}>
+            {[
+              "introduction",
+              "settings",
+              "levels",
+              "vocabulary",
+              "grammar",
+              "examples",
+              "phrases",
+              "notes"
+            ].map((text, index) => {
+              if (text === "settings") {
+                return (
+                  <Can
+                    key={text}
+                    roles={user && user.roles}
+                    perform="course:read-settings"
+                    id={user && user.username}
+                    matchingID={course.owner.username}
+                    yes={() => (
+                      <ListItem
+                        button
+                        component={StyledNavLink}
+                        exact
+                        activeStyle={{
+                          color: "yellow"
+                        }}
+                        to="/course/course-settings"
+                        key={index}>
+                        <Typography>{text}</Typography>
+                      </ListItem>
+                    )}
+                    no={() => null}
+                  />
+                )
+              }
+              return (
+                <ListItem
+                  button
+                  component={StyledNavLink}
+                  exact
+                  activeStyle={{
+                    color: "primary"
+                  }}
+                  to={`/course/course-${text}`}
+                  key={index}>
+                  <Typography>{text}</Typography>
+                </ListItem>
+              )
+            })}
+          </List>
+          <Spacer margin="40px 0 0 0" />
+          <Divider />
+          <Spacer margin="40px 0 0 0" />
+          <div align="center">
+            <Link component={NavLink} to="/courses/created">
+              My Created Courses
+            </Link>
+          </div>
+        </Drawer>
+        <main className={classes.content}>
+          <Helmet>
+            <meta charset="utf-8" />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1, shrink-to-fit=no"
+            />
+            <meta
+              name="description"
+              content="Make direct contact with our team throught our contact information form.  We will do our best to respond in a timely manner.  If you are a business or educational institution this would be an ideal place to shoot a short inquiry."
+            />
+            <meta name="author" content="Isaac Pak" />
+            <title>Utterzone | Courses</title>
+            <link rel="canonical" href="https://utter.zone/courses" />
+          </Helmet>
+          {routes.map(route => (
+            <SubRoutes key={route.path} {...route} />
+          ))}
+        </main>
+      </div>
     )
   }
 }
