51184cb63cc9113f822acb30a33d731079c48bfa
pak11273 <pak11273@gmail.com>
2019-05-07 14:04:21 +0000

    server deployed

--- packages/server/src/middleware/index.js
+++ packages/server/src/middleware/index.js
@@ -9,7 +9,12 @@ import passport from "passport"
 import config from "../config"
 
 export default app => {
-  var whitelist = ["https://utterzone.com", "http://192.168.68.8:8080"]
+  var whitelist = [
+    "https://utterzone.com:80",
+    "https://utterzone.com:443",
+    "https://utterzone.com",
+    "http://192.168.68.8:8080"
+  ]
   var corsOptions = {
     credentials: true,
     origin: (origin, callback) => {
