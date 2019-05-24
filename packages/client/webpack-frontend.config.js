ea9d6a3fd7ff7378986721bcb471f82a88daa58e
pak11273 <pak11273@gmail.com>
2019-05-24 23:03:31 +0000

    shaved down webpack

--- packages/client/webpack-frontend.config.js
+++ packages/client/webpack-frontend.config.js
@@ -1,36 +1,25 @@
 const path = require("path")
 const CompressionPlugin = require("compression-webpack-plugin")
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
-  .BundleAnalyzerPlugin
 // puts our index.html in the output folder and adds a <script> tag including bundle.js
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 // informs webpack to bundle in production
 const webpack = require("webpack")
 const ProgressBarPlugin = require("progress-bar-webpack-plugin")
-const nodeExternals = require("webpack-node-externals")
 const Dotenv = require("dotenv-webpack")
-const WebappWebpackPlugin = require("webapp-webpack-plugin")
 
 module.exports = env => {
-  const {getIfUtils, removeEmpty} = require("webpack-config-utils")
-  const {ifProd, ifNotProd} = getIfUtils(env)
   return {
     context: path.resolve(__dirname, "src"),
     entry: {
       app: "./App.js"
     },
-    // ['./App.js'],
     output: {
       path: path.join(__dirname, "dist"),
       filename: "bundle.[name].[hash].js",
-      publicPath: "/", // use with historyApiFallback
-      pathinfo: ifNotProd() // for dev, makes comments for files in browser devtools
+      publicPath: "/" // use with historyApiFallback
     },
     devtool: env.prod ? "source-map" : "eval",
     watchOptions: {
-      /*   aggregateTimeout: 30, */
-      /* watch: true, */
-      /* poll: 100, */
       ignored: /node_modules/
     },
     devServer: {
@@ -96,25 +85,11 @@ module.exports = env => {
         },
         {
           test: /\.css$/,
-          // exclude: /node_modules/,
           use: ["cache-loader", "style-loader", "css-loader"]
         },
         {
           test: /\.(jpe?g|png|gif|svg|ico|mp3)$/i,
-          use: [
-            {loader: "file-loader?name=[name].[ext]"}
-            // {
-            //   loader: 'image-webpack-loader',
-            //   // This loader reduces image size by half
-            //   // Specify enforce: 'pre' to apply the loader
-            //   // before url-loader/svg-url-loader
-            //   // and not duplicate it in rules with them
-            //   options: {
-            //     enforce: 'pre',
-            //     bypassOnDebug: true
-            //   }
-            // }
-          ]
+          use: [{loader: "file-loader?name=[name].[ext]"}]
         },
         {
           test: /\.(eot|ttf|woff|woff2)$/,
@@ -134,68 +109,23 @@ module.exports = env => {
         }
       ]
     },
-    // bundles npm packages e.g. npm.react-dom.899sadfhj4.js
-    // visitors will never download the same package twice!
-    optimization: {
-      runtimeChunk: "single",
-      splitChunks: {
-        chunks: "all",
-        maxInitialRequests: Infinity,
-        minSize: 0,
-        cacheGroups: {
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name(module) {
-              // get the name. E.g. node_modules/packageName/not/this/part.js
-              // or node_modules/packageName
-              const packageName = module.context.match(
-                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
-              )[1]
-
-              // npm package names are URL-safe, but some servers don't like @ symbols
-              return `npm.${packageName.replace("@", "")}`
-            }
-          }
-        }
-      }
-    },
-    plugins: removeEmpty([
-      ifNotProd(
-        new BundleAnalyzerPlugin({
-          analyzerMode: "server",
-          analyzerHost: "0.0.0.0"
-        })
-      ),
+    plugins: [
       new Dotenv(),
       new ProgressBarPlugin(),
       new webpack.HashedModuleIdsPlugin(), // so that file hashes don't change unexpectedly:w
-      ifProd(
-        new webpack.DefinePlugin({
-          // <-- key to reducing React's size
-          "process.env": {
-            NODE_ENV: JSON.stringify("production")
-          }
-        })
-      ),
       new webpack.HotModuleReplacementPlugin(),
       new webpack.NoEmitOnErrorsPlugin(),
       new HtmlWebpackPlugin({
         template: "index.html"
-        // inject: 'head'
-        /* favicon: "./src/favicons/favicon.ico" */
-        // inject: false
       }),
-      new WebappWebpackPlugin("./assets/images/logo.png"), // svg works too!
-      // ifProd(new webpack.optimize.UglifyJsPlugin({sourceMap: true})), //minify everything
       new CompressionPlugin({
         filename: "[path].gz[query]",
         algorithm: "gzip",
         test: /\.js$|\.css$|\.html$|\.eot?.+$|\.ttf?.+$|\.woff?.+$|\.svg?.+$/,
         threshold: 10240,
         minRatio: 0.8
-      }),
-      new webpack.optimize.AggressiveMergingPlugin() //Merge chunks
-    ]),
+      })
+    ],
     node: {
       fs: "empty",
       net: "empty",
--- packages/client/webpack.config.js
+++ packages/client/webpack.config.js
@@ -1,61 +1,196 @@
 const path = require("path")
+const CompressionPlugin = require("compression-webpack-plugin")
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin
+// puts our index.html in the output folder and adds a <script> tag including bundle.js
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+// informs webpack to bundle in production
 const webpack = require("webpack")
 const ProgressBarPlugin = require("progress-bar-webpack-plugin")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const nodeExternals = require("webpack-node-externals")
+const Dotenv = require("dotenv-webpack")
+const WebappWebpackPlugin = require("webapp-webpack-plugin")
 
 module.exports = env => {
-  console.log("env: ", env)
+  const {getIfUtils, removeEmpty} = require("webpack-config-utils")
+  const {ifProd, ifNotProd} = getIfUtils(env)
   return {
     context: path.resolve(__dirname, "src"),
-    mode: env,
     entry: {
-      main: "./App.js"
+      app: "./App.js"
     },
+    // ['./App.js'],
     output: {
       path: path.join(__dirname, "dist"),
       filename: "bundle.[name].[hash].js",
-      publicPath: "/" // use with historyApiFallback
+      publicPath: "/", // use with historyApiFallback
+      pathinfo: ifNotProd() // for dev, makes comments for files in browser devtools
+    },
+    devtool: env.prod ? "source-map" : "eval",
+    watchOptions: {
+      /*   aggregateTimeout: 30, */
+      /* watch: true, */
+      /* poll: 100, */
+      ignored: /node_modules/
     },
     devServer: {
       stats: "minimal",
       disableHostCheck: true,
-      historyApiFallback: true // redirects all browser requests to publicPath, then react router takes over.  prevents browser from grabbing assets from the server while using webdevserver.
+      historyApiFallback: true, // redirects all browser requests to publicPath, then react router takes over.  prevents browser from grabbing assets from the server while using webdevserver.
+      proxy: [
+        {
+          context: [
+            "/admin/uploadFile",
+            "/auth",
+            "/api",
+            "/api/users",
+            "/api/channels",
+            "/api/comments",
+            "/api/courses",
+            "/api/events",
+            "/api/vocab",
+            "/api/zones",
+            "/api/test",
+            "/cdn",
+            "/graphql",
+            "/socket.io",
+            "/lions",
+            "/tigers",
+            "/mail"
+          ],
+          target: "http://192.168.68.8:3010",
+          ws: true
+        }
+      ]
+    },
+    resolve: {
+      alias: {"react-dom": "@hot-loader/react-dom"},
+      extensions: [".mjs", ".js", ".jsx"] // common extensions
+    },
+    stats: {
+      colors: true,
+      reasons: true,
+      chunks: true
     },
     module: {
       rules: [
         {
-          test: /\.(js|jsx)$/,
-          include: path.resolve(__dirname, "src"),
+          test: /\.mjs$/,
+          include: /node_modules/,
+          type: "javascript/auto"
+        },
+        {
+          test: /\.jsx?$/,
           exclude: /node_modules/,
-          loader: "babel-loader"
+          use: ["cache-loader", "babel-loader"]
         },
         {
-          test: /\.m?js$/,
-          loader: "babel-loader"
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ["babel-loader", "eslint-loader"]
         },
         {
-          test: /\.(jpe?g|png|gif|svg|ico|mp3)$/i,
-          loader: "file-loader?name=[name].[ext]"
+          test: /\.(graphql|gql)$/,
+          exclude: /node_modules/,
+          use: [{loader: "graphql-import-loader"}]
         },
         {
           test: /\.css$/,
           // exclude: /node_modules/,
-          use: ["style-loader", "css-loader"]
+          use: ["cache-loader", "style-loader", "css-loader"]
+        },
+        {
+          test: /\.(jpe?g|png|gif|svg|ico|mp3)$/i,
+          use: [
+            {loader: "file-loader?name=[name].[ext]"}
+            // {
+            //   loader: 'image-webpack-loader',
+            //   // This loader reduces image size by half
+            //   // Specify enforce: 'pre' to apply the loader
+            //   // before url-loader/svg-url-loader
+            //   // and not duplicate it in rules with them
+            //   options: {
+            //     enforce: 'pre',
+            //     bypassOnDebug: true
+            //   }
+            // }
+          ]
+        },
+        {
+          test: /\.(eot|ttf|woff|woff2)$/,
+          use: [
+            // DO NOT cache this
+            {
+              loader: "file-loader",
+              options: {
+                name: "[name].[ext]"
+              }
+            }
+          ]
+        },
+        {
+          test: /react-icons\/(.)*(.js)$/,
+          use: ["cache-loader", "babel-loader"]
         }
       ]
     },
+    // bundles npm packages e.g. npm.react-dom.899sadfhj4.js
+    // visitors will never download the same package twice!
+    optimization: {
+      runtimeChunk: "single",
+      splitChunks: {
+        chunks: "all",
+        maxInitialRequests: Infinity,
+        minSize: 0,
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              // get the name. E.g. node_modules/packageName/not/this/part.js
+              // or node_modules/packageName
+              const packageName = module.context.match(
+                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+              )[1]
+
+              // npm package names are URL-safe, but some servers don't like @ symbols
+              return `npm.${packageName.replace("@", "")}`
+            }
+          }
+        }
+      }
+    },
+    plugins: removeEmpty([
+      ifNotProd(
+        new BundleAnalyzerPlugin({
+          analyzerMode: "server",
+          analyzerHost: "0.0.0.0"
+        })
+      ),
+      new Dotenv(),
+      new ProgressBarPlugin(),
+      new webpack.HashedModuleIdsPlugin(), // so that file hashes don't change unexpectedly:w
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoEmitOnErrorsPlugin(),
+      new HtmlWebpackPlugin({
+        template: "index.html"
+        // inject: 'head'
+        /* favicon: "./src/favicons/favicon.ico" */
+        // inject: false
+      }),
+      new WebappWebpackPlugin("./assets/images/logo.png"), // svg works too!
+      new CompressionPlugin({
+        filename: "[path].gz[query]",
+        algorithm: "gzip",
+        test: /\.js$|\.css$|\.html$|\.eot?.+$|\.ttf?.+$|\.woff?.+$|\.svg?.+$/,
+        threshold: 10240,
+        minRatio: 0.8
+      }),
+      new webpack.optimize.AggressiveMergingPlugin() //Merge chunks
+    ]),
     node: {
       fs: "empty",
       net: "empty",
       dns: "empty"
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: "index.html"
-      }),
-      new ProgressBarPlugin(),
-      /* new webpack.ProgressPlugin() */
-      new webpack.HotModuleReplacementPlugin()
-    ]
+    }
   }
 }
